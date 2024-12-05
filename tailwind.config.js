c48230c41d36ff46c35d323f981a76e36b1d876e
Eva <ekoss.prog@gmail.com>
2024-12-05 19:52:13 +0400

    Add custom animation

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,7 +6,21 @@ module.exports = {
     "./components/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      animation: {
+        "slow-up-down": "updown 8s ease-in-out infinite", // Smooth up-and-down motion
+      },
+      keyframes: {
+        updown: {
+          "0%, 100%": {
+            transform: "translateY(-10px)", // Moves up by 20px
+          },
+          "50%": {
+            transform: "translateY(10px)", // Moves down by 20px
+          },
+        },
+      },
+    },
   },
   plugins: [],
 };
