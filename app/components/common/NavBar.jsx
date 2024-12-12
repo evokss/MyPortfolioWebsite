5f21b83999547f515627416f35015e03bd4d2cc0
Eva <ekoss.prog@gmail.com>
2024-12-12 21:01:38 +0400

    Implement theme switching with icons and styles

--- app/components/common/NavBar.jsx
+++ app/components/common/NavBar.jsx
@@ -1,11 +1,27 @@
 "use client";
 
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import { FiMenu, FiX } from "react-icons/fi";
 import Link from "next/link";
 import { usePathname } from "next/navigation";
 
 const Navbar = () => {
+  const initialTheme = localStorage.getItem("theme") || "dark";
+  const [theme, setTheme] = useState(initialTheme);
+
+  const themeSwitchHandler = (newTheme) => {
+    if (newTheme === "light" || newTheme === "dark") {
+      setTheme(newTheme);
+      localStorage.setItem("theme", newTheme);
+    }
+  };
+
+  useEffect(() => {
+    theme === "dark"
+      ? document.body.classList.add("dark")
+      : document.body.classList.remove("dark");
+  }, [theme]);
+
   const [isOpen, setIsOpen] = useState(false);
   const pathname = usePathname();
 
@@ -17,7 +33,7 @@ const Navbar = () => {
   const isActive = (path) => pathname === path;
 
   return (
-    <nav className="bg-white shadow-md sticky top-0 z-50">
+    <nav className="bg-white dark:bg-gray-900 shadow-md dark:shadow-sm dark:shadow-slate-800 sticky top-0 z-50">
       <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
         <div className="flex justify-between items-center h-16">
           {/* Logo/Brand Icon */}
@@ -33,13 +49,29 @@ const Navbar = () => {
 
           {/* Desktop Links */}
           <div className="hidden md:flex space-x-6">
+            {theme === "light" ? (
+              <img
+                src="/icons/moon.png"
+                className="size-6 transition ease-in-out delay-125 hover:scale-125 cursor-pointer"
+                onClick={() => themeSwitchHandler("dark")}
+                alt="Header Logo"
+              />
+            ) : (
+              <img
+                src="/icons/sun.png"
+                className="size-6 transition ease-in-out delay-125 hover:scale-125 cursor-pointer"
+                onClick={() => themeSwitchHandler("light")}
+                alt="Header Logo"
+              />
+            )}
+
             <Link
               href="/"
               className={`
                 ${
                   isActive("/")
                     ? "text-rose-600"
-                    : "text-gray-800 hover:text-rose-600 transition duration-300"
+                    : "text-gray-800 dark:text-white hover:text-rose-600 transition duration-300"
                 }
               `}
             >
@@ -47,25 +79,25 @@ const Navbar = () => {
             </Link>
             <Link
               href="/development"
-              className="text-gray-800 hover:text-rose-600 transition duration-300"
+              className="text-gray-800 dark:text-white hover:text-rose-600 transition duration-300"
             >
               About
             </Link>
             <Link
               href="/development"
-              className="text-gray-800 hover:text-rose-600 transition duration-300"
+              className="text-gray-800 dark:text-white hover:text-rose-600 transition duration-300"
             >
               Projects
             </Link>
             <Link
               href="/development"
-              className="text-gray-800 hover:text-rose-600 transition duration-300"
+              className="text-gray-800 dark:text-white hover:text-rose-600 transition duration-300"
             >
               CV
             </Link>
             <Link
               href="/development"
-              className="text-gray-800 hover:text-rose-600 transition duration-300"
+              className="text-gray-800 dark:text-white hover:text-rose-600 transition duration-300"
             >
               Contact Me
             </Link>
@@ -75,7 +107,7 @@ const Navbar = () => {
           <div className="md:hidden flex items-center">
             <button
               onClick={toggleMenu}
-              className="text-gray-800 focus:outline-none"
+              className="text-gray-800 dark:text-white focus:outline-none"
             >
               {isOpen ? <FiX size={24} /> : <FiMenu size={24} />}
             </button>
@@ -85,15 +117,15 @@ const Navbar = () => {
 
       {/* Mobile Links */}
       {isOpen && (
-        <div className="md:hidden bg-white border-t border-gray-200">
+        <div className="md:hidden bg-white dark:bg-gray-900 border-t border-gray-200">
           <Link
             href="/"
             className={`
               block px-4 py-2 
               ${
                 isActive("/")
-                  ? "text-rose-600 bg-gray-100"
-                  : "text-gray-800 hover:bg-gray-100"
+                  ? "text-rose-600 bg-gray-100 dark:bg-gray-900"
+                  : "text-gray-800 dark:text-white hover:bg-gray-100 dark:hover:bg-gray-800"
               }
             `}
           >
@@ -101,25 +133,25 @@ const Navbar = () => {
           </Link>
           <Link
             href="/development"
-            className="block px-4 py-2 text-gray-800 hover:bg-gray-100"
+            className="block px-4 py-2 text-gray-800 dark:text-white hover:bg-gray-100 dark:hover:bg-gray-800"
           >
             About
           </Link>
           <Link
             href="/development"
-            className="block px-4 py-2 text-gray-800 hover:bg-gray-100"
+            className="block px-4 py-2 text-gray-800 dark:text-white hover:bg-gray-100 dark:hover:bg-gray-800"
           >
             Projects
           </Link>
           <Link
             href="/development"
-            className="block px-4 py-2 text-gray-800 hover:bg-gray-100"
+            className="block px-4 py-2 text-gray-800 dark:text-white hover:bg-gray-100 dark:hover:bg-gray-800"
           >
             CV
           </Link>
           <Link
             href="/development"
-            className="block px-4 py-2 text-gray-800 hover:bg-gray-100"
+            className="block px-4 py-2 text-gray-800 dark:text-white hover:bg-gray-100 dark:hover:bg-gray-800"
           >
             Contact Me
           </Link>
