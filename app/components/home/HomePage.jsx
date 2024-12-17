b93738000b0cad3e7671040521e026014ee239f3
Eva <ekoss.prog@gmail.com>
2024-12-17 19:11:38 +0400

    Integrate StarryBackground into HomePage

--- app/components/home/HomePage.jsx
+++ app/components/home/HomePage.jsx
@@ -1,11 +1,11 @@
-import React from "react";
+import { StarryBackground } from "../../utils/starryBackground";
 import ProfileSection from "./ProfileSection";
 
 const HomePage = () => {
   return (
-    <div className="flex min-h-screen items-center bg-slate-50 lg:-mt-10 dark:bg-gray-900 dark:text-white">
+    <StarryBackground>
       <ProfileSection />
-    </div>
+    </StarryBackground>
   );
 };
 
