fd6d5745cb22f183f1626ced27ed0af37e4ceb4f
Eva <ekoss.prog@gmail.com>
2024-12-25 17:33:51 +0400

    Add projects data file for portfolio integration
    - Created a new file exporting a structured array of project objects.
    - Included project details such as name, description, technologies, GitHub links, deploy links, and image URLs.
    - Prepared data for seamless integration into portfolio components.

---
+++ app/components/projects/projects.js
@@ -0,0 +1,64 @@
+export const projects = [
+  {
+    id: 1,
+    name: "My Fav Videos",
+    description:
+      "A dynamic video grid application, featuring a responsive layout, interactive components, and seamless integration of favorite channels and video details for an engaging user experience.",
+    technologies: ["React", "Tailwind", "Next.js"],
+    githubLink: "https://github.com/evokss/MyFavVideos",
+    deployLink: "",
+    imageUrl: "/icons/movie.png",
+  },
+  {
+    id: 2,
+    name: "Meme Factory",
+    description:
+      "Generates random meme images from a preloaded dataset, and view the resulting meme on the screen. The app dynamically updates the displayed meme with each click, providing an interactive experience.",
+    technologies: ["React", "JavaScript", "HTML", "CSS", "Node.js"],
+    githubLink: "https://github.com/evokss/MemeFactory",
+    deployLink: "",
+    imageUrl: "/icons/troll-face.png",
+  },
+  {
+    id: 3,
+    name: "Classic Rock Paper Scissors",
+    description:
+      "The game dynamically updates the interface with real-time results, tracks player scores persistently using localStorage, and employs logic to simulate computer moves and outcomes.",
+    technologies: ["JavaScript", "HTML", "CSS"],
+    githubLink: "https://github.com/evokss/MyRockPaperScissors",
+    deployLink: "",
+    imageUrl: "/icons/rock-paper-scissors.png",
+  },
+  {
+    id: 4,
+    name: "YouTube Clone",
+    description:
+      "The project replicates the core design of YouTube's interface, featuring a structured video grid, navigation bar, and user-friendly design elements, all built without the use of JavaScript.",
+    technologies: ["Bootstrap", "HTML", "CSS"],
+    githubLink: "https://github.com/evokss/MyRockPaperScissors",
+    deployLink: "https://evokss.github.io/MyYouTubeClone.github.io/",
+    imageUrl: "/icons/youtube.png",
+  },
+  {
+    id: 5,
+    name: "Airbnb Clone",
+    description:
+      "This project display online experiences with badges for availability and location. It leverages props, mapping techniques, and conditional rendering to create a reusable and scalable codebase.",
+    technologies: ["React", "HTML", "CSS", "JavaScript"],
+    githubLink:
+      "https://github.com/evokss/ReactProjects/tree/main/airbnb-clone",
+    deployLink: "",
+    imageUrl: "/icons/airbnb.png",
+  },
+  {
+    id: 6,
+    name: "React Fun Facts",
+    description:
+      "The game dynamically updates the interface with real-time results, tracks player scores persistently using localStorage, and employs logic to simulate computer moves and outcomes.",
+    technologies: ["React", "JavaScript", "HTML", "CSS"],
+    githubLink:
+      "https://github.com/evokss/ReactProjects/tree/main/react-info-page",
+    deployLink: "https://react-info-page-evas-projects.netlify.app/",
+    imageUrl: "/icons/react.png",
+  },
+];
