a0409dd68e2ef7801953019e0b744401c9a84cb8
Eva <ekoss.prog@gmail.com>
2025-01-12 18:50:53 +0400

    Add "React Online Market" project and update other project details

--- app/components/projects/projects.js
+++ app/components/projects/projects.js
@@ -4,23 +4,33 @@ export const projects = [
     name: "My Fav Videos",
     description:
       "A dynamic video grid application, featuring a responsive layout, interactive components, and seamless integration of favorite channels and video details for an engaging user experience.",
-    technologies: ["React", "Tailwind", "Next.js"],
+    technologies: ["React", "Tailwind", "Next.js", "HTML", "CSS"],
     githubLink: "https://github.com/evokss/MyFavVideos",
     deployLink: "https://my-fav-videos.vercel.app/",
     imageUrl: "/icons/movie.png",
   },
   {
     id: 2,
+    name: "React Online Market",
+    description:
+      "A modern e-commerce web application built with React, featuring a catalog of articles/products, user authentication, shopping basket functionality, and user profiles. The application implements a robust state management system combining Redux and custom hooks, with support for internationalization.",
+    technologies: ["React", "React Router v6", "Redux", "HTML", "CSS"],
+    githubLink: "https://github.com/evokss/ReactOnlineMarket",
+    deployLink: "https://react-products-market.vercel.app/",
+    imageUrl: "/icons/shop.png",
+  },
+  {
+    id: 3,
     name: "Meme Factory",
     description:
       "Generates random meme images from a preloaded dataset, and view the resulting meme on the screen. The app dynamically updates the displayed meme with each click, providing an interactive experience.",
-    technologies: ["React", "JavaScript", "HTML", "CSS", "Node.js"],
+    technologies: ["React", "Node.js", "HTML", "CSS"],
     githubLink: "https://github.com/evokss/MemeFactory",
     deployLink: "https://memes-factory.vercel.app/",
     imageUrl: "/icons/troll-face.png",
   },
   {
-    id: 3,
+    id: 4,
     name: "Classic Rock Paper Scissors",
     description:
       "The game dynamically updates the interface with real-time results, tracks player scores persistently using localStorage, and employs logic to simulate computer moves and outcomes.",
@@ -30,7 +40,7 @@ export const projects = [
     imageUrl: "/icons/rock-paper-scissors.png",
   },
   {
-    id: 4,
+    id: 5,
     name: "RetroTube",
     description:
       "The project replicates the core design of YouTube's interface, featuring a structured video grid, navigation bar, and user-friendly design elements, all built without the use of JavaScript.",
@@ -40,22 +50,22 @@ export const projects = [
     imageUrl: "/icons/youtube.png",
   },
   {
-    id: 5,
+    id: 6,
     name: "Airbnb Experiences Mobile Clone",
     description:
       "A mobile-first app showcasing online experiences with badges for availability and location. It employs reusable components, mapping techniques, and conditional rendering for scalable, responsive design.",
-    technologies: ["React", "HTML", "CSS", "JavaScript"],
+    technologies: ["React", "HTML", "CSS"],
     githubLink:
       "https://github.com/evokss/ReactProjects/tree/main/airbnb-clone",
     deployLink: "https://my-air-clone.vercel.app/",
     imageUrl: "/icons/airbnb.png",
   },
   {
-    id: 6,
+    id: 7,
     name: "React Fun Facts Mobile App",
     description:
       "A mobile-first application presenting essential React facts in a responsive and user-friendly interface. It features a structured design with a reusable Navbar and Main components.",
-    technologies: ["React", "JavaScript", "HTML", "CSS"],
+    technologies: ["React", "HTML", "CSS"],
     githubLink:
       "https://github.com/evokss/ReactProjects/tree/main/react-info-page",
     deployLink: "https://react-info-page-evas-projects.netlify.app/",
